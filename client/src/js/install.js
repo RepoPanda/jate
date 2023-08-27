5859e541baaaae9baf50911ab54f9af85e79ddcc
RepoPanda <124810474+RepoPanda@users.noreply.github.com>
2023-08-26 23:28:35 -0700

    install.js beforeinstallprompt event handler has been updated

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,8 +1,12 @@
 const butInstall = document.getElementById('buttonInstall');
+butInstall.classList.add("hidden")
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    window.deferredPrompt = event;
+    butInstall.classList.remove("hidden", false);
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {});
